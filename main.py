ef68ff80ce89f97551d0b9e38bbda55b68f25682
stefan <dev@2904.cc>
2016-03-15 00:41:04 +0100

    Fixed r3bot referencing

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 
 from flask import Flask, request
 import config
-import r3bot
+from r3botimport r3bot
 
 app = Flask(__name__)
-bot =r3bot(config.APITOKEN)
+bot = r3bot(config.APITOKEN)
 bot.initHook(config.HOOKURL)
 
 @app.route('/hook/', methods=['POST'])
