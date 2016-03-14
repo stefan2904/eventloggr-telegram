af5a9a01f713b52b485235dd9dc9fa6937f48104
stefan <dev@2904.cc>
2016-03-15 00:52:05 +0100

    Added more logging to r3bot

--- r3bot.py
+++ r3bot.py
@@ -13,6 +13,7 @@ class r3bot():
         self.log('initializing r3bot ...')
         self.setupBot(apitoken)
         self.__loadBotState()
+        self.log('initializing r3bot done!')
 
     def log(self, text):
         print text
@@ -25,15 +26,19 @@ class r3bot():
         self.bot.update_bot_info().wait()
 
     def initHook(self, hook):
+        self.log('initializing callback hook!')
         self.bot.set_webhook(url=hook)
+        self.log('initialized callback hook!')
 
     def processHookRequest(self, request):
+        self.log('received request on HOOK:')
         print request
 
     def __saveBotState(self):
         """
         Saves the bot's state to state.json file.
         """
+        self.log('saving bot state ...')
         with open('state.json', 'w') as fp:
             json.dump(self.state, fp)
 
@@ -41,6 +46,7 @@ class r3bot():
         """
         Loads the bot's state from state.json file.
         """
+        self.log('loading bot state ...')
         try:
             with open('state.json', 'r') as fp:
                 self.state = json.load(fp)
